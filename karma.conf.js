85a27db0dbc3789780ea531a19a699b9fa771432
Yosi Attias <yosi@bringg.com>
2016-12-18 12:04:05 +0200

    Tests & Coverage
    1. Upgrading karma conf
    2. Setting up `npm test` script
    3. Circle CI - coverage & test summary

--- karma.conf.js
+++ karma.conf.js
@@ -1,17 +1,23 @@
 // Karma configuration
-// Generated on Tue Mar 15 2016 11:46:47 GMT+0200 (IST)
+// http://karma-runner.github.io/0.12/config/configuration-file.html
+// Generated on 2015-10-11 using
+// generator-karma 1.0.0
 
 module.exports = function(config) {
-  config.set({
+  'use strict';
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
+  config.set({
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+    // base path, that will be used to resolve files and exclude
     basePath: '',
 
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
-
+    // testing framework to use (jasmine/mocha/qunit/...)
+    // as well as any additional frameworks (requirejs/chai/sinon/...)
+    frameworks: [
+      'jasmine'
+    ],
 
     // list of files / patterns to load in the browser
     files: [
@@ -22,57 +28,78 @@ module.exports = function(config) {
       'bower_components/faker.js/build/build/faker.js'
     ],
 
-
-    // list of files to exclude
-    exclude: [
-    ],
-
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      // source files, that you wanna generate coverage for
+      // do not include tests or libraries
+      // (these files will be instrumented by Istanbul)
+      'BringgSDK.js': ['coverage']
     },
 
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
-
+    // list of files / patterns to exclude
+    exclude: [
+    ],
 
     // web server port
     port: 9876,
 
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: [
+      'PhantomJS'
+    ],
 
     // Which plugins to enable
     plugins: [
-      "karma-phantomjs-launcher",
-      "karma-jasmine"
+      'karma-phantomjs-launcher',
+      'karma-coverage',
+      'karma-jasmine',
+      'karma-notify-reporter',
+      'karma-mocha-reporter',
+      'karma-junit-reporter'
     ],
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    junitReporter: {
+      outputDir: 'junit_results'
+    },
 
-    // Concurrency level
-    // how many browser should be started simultanous
-    concurrency: Infinity
-  })
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: true,
+
+    colors: true,
+
+    reporters: [
+      'coverage',
+      'notify',
+      'mocha',
+      'junit'
+    ],
+
+    coverageReporter: {
+      type : 'html',
+      dir : 'coverage/'
+    },
+
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // Uncomment the following lines if you are using grunt's server to run the tests
+    // proxies: {
+    //   '/': 'http://localhost:9000/'
+    // },
+    // URL root prevent conflicts with the site root
+    // urlRoot: '_karma_'
+    notifyReporter: {
+      reportEachFailure: true, // Default: false, Will notify on every failed sepc
+      reportSuccess: true // Default: true, Will notify when a suite was successful
+    }
+  });
 };
